26626983965e25ec20da542ea51d51ee638205d8
KHALID SHAIKH <ks0903525@gmail.com>
2025-08-09 12:43:44 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 import express from 'express';
 import connectDB from './config/db.js'
 import dotenv from 'dotenv';
-// import cors from 'cors';
-// import authRoutes from './routes/adminRoutes.js';
-// import noticeRoutes from './routes/noticeRoutes.js';
-// import blogRoutes from './routes/blogRoutes.js';
-// import mediaRoutes from './routes/mediaRoutes.js';
-// import topperRoutes from './routes/topperRoutes.js';
-// import activityRoutes from './routes/activityRoutes.js';
-// import newsRoutes from './routes/newsRoutes.js'
-// import path from 'path';
-// import { fileURLToPath } from 'url';
+import cors from 'cors';
+import authRoutes from './routes/adminRoutes.js';
+import noticeRoutes from './routes/noticeRoutes.js';
+import blogRoutes from './routes/blogRoutes.js';
+import mediaRoutes from './routes/mediaRoutes.js';
+import topperRoutes from './routes/topperRoutes.js';
+import activityRoutes from './routes/activityRoutes.js';
+import newsRoutes from './routes/newsRoutes.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// app.use(cors({
-//     origin:["http://localhost:3000","http://localhost:3001","https://maf-iti.nexcorealliance.com","https://iti-admin.nexcorealliance.com"],
-//     credentials:true
-// }));
+app.use(cors({
+    origin:["http://localhost:3000","http://localhost:3001","https://maf-iti.nexcorealliance.com","https://iti-admin.nexcorealliance.com"],
+    credentials:true
+}));
 
 app.use(express.json());
 
 connectDB();
 
-// app.use('/api/admin/auth', authRoutes);
-// app.use('/api/admin/notices', noticeRoutes);
-// app.use('/api/admin/blogs', blogRoutes);
-// app.use('/api/admin/media', mediaRoutes);
-// app.use('/api/admin/toppers', topperRoutes);
-// app.use('/api/admin/activities', activityRoutes);
-// app.use('/api/admin/news', newsRoutes);
+app.use('/api/admin/auth', authRoutes);
+app.use('/api/admin/notices', noticeRoutes);
+app.use('/api/admin/blogs', blogRoutes);
+app.use('/api/admin/media', mediaRoutes);
+app.use('/api/admin/toppers', topperRoutes);
+app.use('/api/admin/activities', activityRoutes);
+app.use('/api/admin/news', newsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
