f99ea62ebb7de731d84f7bfff754f41619ec91d0
Abdul <abdulshafey24@gmail.com>
2025-08-10 02:51:34 +0530

    chore: update MONGO_URI in .env, refactor adminNotice and topper routes, and adjust Topper model

--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -1,19 +1,24 @@
-import express from 'express';
-import verifyAdmin from '../middlewares/verifyAdmin.js';
+import express from "express";
+import verifyAdmin from "../middlewares/verifyAdmin.js";
 import {
   createNotice,
   getNotices,
   updateNotice,
-  deleteNotice
-} from '../controllers/adminNotice.js';
+  deleteNotice,
+} from "../controllers/adminNotice.js";
 
 const router = express.Router();
 
 // router.use(verifyAdmin);
 
-router.post('/',verifyAdmin, createNotice);
-router.get('/', getNotices);
-router.put('/:id', verifyAdmin,updateNotice);
-router.delete('/:id', verifyAdmin, deleteNotice);
+// router.post('/',verifyAdmin, createNotice);
+// router.get('/', getNotices);
+// router.put('/:id', verifyAdmin,updateNotice);
+// router.delete('/:id', verifyAdmin, deleteNotice);
+
+router.post("/", createNotice);
+router.get("/", getNotices);
+router.put("/:id", updateNotice);
+router.delete("/:id", deleteNotice);
 
 export default router;
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:3001",
       "https://maf-iti.nexcorealliance.com",
       "https://iti-admin.nexcorealliance.com",
-    ],
+    ], // your frontend domain
     credentials: true,
   })
 );
@@ -41,5 +41,5 @@ app.use("/api/admin/toppers", topperRoutes);
 app.use("/api/admin/activities", activityRoutes);
 // app.use("/api/admin/news", newsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
