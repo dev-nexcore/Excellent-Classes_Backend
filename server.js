882065eb361b5fc2a0a37626f7e7ac935f2d6c86
Sknagma5233 <sknagma5233@gmail.com>
2025-08-11 16:29:57 +0530

    fix: update server port from 5000 to 5001

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.use("/api/admin/toppers", topperRoutes);
 app.use("/api/admin/activities", activityRoutes);
 // app.use("/api/admin/news", newsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
